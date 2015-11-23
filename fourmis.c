b11ec340407b6ce2b0bc31facc19a54b55cfda03
j4r0g <j4r0gz@gmail.com>
2015-11-23 16:00:41 +0100

    suppression d'un fichier mal placé

--- fourmis.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <maths.h>
-
-typedef enum t_sexe {MALE, FEMELLE};
-typedef struct {int num;  t_sexe sexe; int nourriture; int age;} t_fourmi;
-
-void prochain_deplacement (t_fourmi fourmi, int pos_x, int pos_y) {
-	if (t_lab[pos_x-1][pos_y-1] == 
