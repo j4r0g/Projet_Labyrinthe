d4dc3659e22340e673f39bb1d46faba2347378b4
lboisson <boisson.leo@gmail.com>
2015-11-23 18:19:34 +0100

    gen pseudo-random

--- fonctions/labyrinthe.c
+++ fonctions/labyrinthe.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h> 
 #define X 20 //Définit la taille du labyrinthe
 #define Y 20
 
@@ -10,12 +11,21 @@ typedef struct {t_discover decouvert; t_etat etat ; int insecte ;} t_lab ;
 t_lab lab[X][Y];
 
 
+//*			fonction qui renvoie un nombre aleatoire entre 0 et 99			*//
+int nbr_rand(){
+	int m=100; 
+	int nombre;
+	nombre = rand()%m;
+	return nombre;
+}
+
+
+//*			initialisation du labyrinthe à partir d'un .txt			*//
 void init_lab (t_lab lab[X][Y]){
 	int tmp;
 	int i,j;
 	FILE * fic1;
 	fic1 = fopen("../doc/lab_test.txt", "r");
-	
 	
 	while(!feof(fic1))
 	{	
@@ -23,13 +33,76 @@ void init_lab (t_lab lab[X][Y]){
 			for(j =0; j<Y; j++){				
 				fscanf(fic1, "%d ", &tmp);
 				lab[i][j].etat = tmp;
-				lab[i][j].decouvert = seen;
+				lab[i][j].decouvert = unseen;
 			}
 		}
 	}
 	fclose(fic1);
 }
 
+
+//*			initialisation pseudo-aléatoire du labyrinthe			*//
+void init_lab_rand (t_lab lab[X][Y]){
+	int i,j, x, y, x_tmp, y_tmp, compteur_ilot, branche, rand_branche;
+	int nbr_ilot=0;
+	
+	for(i =0; i<X; i++){
+		for(j =0; j<Y; j++){				
+			if(i==0 || i==19 || j==0 || j==19){
+				lab[i][j].etat = mur;
+			}
+			lab[i][j].decouvert = seen;
+		}
+	}
+	
+	nbr_ilot = (nbr_rand()/10) +4;
+	for(compteur_ilot=0; compteur_ilot<nbr_ilot; compteur_ilot++){
+		x = nbr_rand()/5;
+		y = nbr_rand()/5;
+		while( lab[x][y].etat != vide){
+			x = nbr_rand()/5;
+			y = nbr_rand()/5;
+		}
+		lab[x][y].etat = mur;
+		
+		for(i=-1; i<=1; i++){
+			for(j=-1; j<=1; j++){
+				x_tmp = x+i;
+				y_tmp = y+j;
+				branche = 1;
+				while(branche == 1){
+					if(lab[x_tmp][y_tmp].etat == mur){
+						branche =0;
+					}
+					else{
+						rand_branche = nbr_rand();
+						if(rand_branche<50){
+							lab[x_tmp][y_tmp].etat = mur;
+							if(x_tmp<0){
+								x_tmp--;
+							}
+							else if(x_tmp>0){
+								x_tmp++;
+							}
+							if(y_tmp<0){
+								y_tmp--;
+							}
+							else if(y_tmp>0){
+								y_tmp++;
+							}
+						}
+						else{
+							branche = 0;
+						}
+					}
+				}
+			}
+		}
+		
+	}
+}
+
+//*			affichage du labyrinthe en ASCII			*//
 void afficher_lab(t_lab lab[X][Y]){
 	
 	int char_unseen = 63;
@@ -42,12 +115,12 @@ void afficher_lab(t_lab lab[X][Y]){
 	t_discover decouvert_tmp;
 	int i,j;
 	
-	printf("\n");
+	printf("\n\n");
 	for(i =0; i<X; i++){
 		for(j =0; j<Y; j++){
 			etat_tmp = lab[i][j].etat;
 			decouvert_tmp = lab[i][j].decouvert;
-			if(decouvert_tmp == 0){
+			if(decouvert_tmp == 0){ 
 				printf("%c ", char_unseen);
 			}
 			else{
@@ -71,10 +144,13 @@ void afficher_lab(t_lab lab[X][Y]){
 		}
 		printf("\n");
 	}
+	printf("\n\n");
 }
 
+
 int main(){
-	init_lab(lab);
+	srand(time(NULL));
+	init_lab_rand(lab);
 	afficher_lab(lab);
 	return 0;
 }
