cb868f7761597ca1d495f544ac4503559e180dd3
keryann <keryannbussereau@gmail.com>
2015-11-26 09:27:46 +0100

    génération aléatoire des fourmis

--- fonctions/contigue.c
+++ fonctions/contigue.c
@@ -7,9 +7,31 @@
 //typedef enum {vide=0, mur=1, food=2, insecte=3} t_etat;
 //typedef struct {t_discover decouvert; t_etat etat; int insecte;} t_lab;
 
-void combat(int x, int y, int a, int b) {
-	if(fourmi[lab[x][y].insecte].sexe!=fourmi[lab[a][b].insecte].sexe)
+// maxx et maxy représentent la taille du labyrinthe
+void generation(int maxx, int maxy, int bouffe, int dureevie) {
+	srand(time(NULL));
+	int nombrex, nombrey, sexe;
+	nombrex = rand()%maxx;
+	nombrey = rand()%maxy;
+	while(lab[nombrex][nombrey].etat!=vide && lab[nombrex][nombrey].decouvert!=seen){
+		nombrex = rand()%maxx;
+		nombrey = rand()%maxy;
+	}
+	lab[nombrex][nombrey].etat=insecte;
+	sexe=rand()%100;
+	if(sexe>=0 && sexe<50)
+		fourmi[lab[nombrex][nombrey].insecte].sexe = MALE;
+	else
+		fourmi[lab[nombrex][nombrey].insecte].sexe = FEMELLE;
 
+	fourmi[lab[nombrex][nombrey].insecte].nourriture = bouffe;
+	fourmi[lab[nombrex][nombrey].insecte].age = dureevie;
+}
+
+// x et y correspondent à l'emplacement de la première fourmi et a et b à l'emplacement de la deuxième
+void combat(int x, int y, int a, int b, int maxx, int maxy, int bouffe, int dureevie) {
+	if(fourmi[lab[x][y].insecte].sexe!=fourmi[lab[a][b].insecte].sexe)
+		generation(maxx, maxy, bouffe, dureevie);
 	else {
 		if(fourmi[lab[x][y].insecte].age>fourmi[lab[a][b].insecte].age)
 			lab[x][y].etat=food;
