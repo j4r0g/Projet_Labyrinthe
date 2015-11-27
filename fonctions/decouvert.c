9053c5809f1a97c2ddfd698a4190992f5eedfaea
keryann <keryannbussereau@gmail.com>
2015-11-27 10:13:36 +0100

    ajout fonction de découverte du labyrinthe

---
+++ fonctions/decouvert.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef enum {MALE, FEMELLE} t_sexe;
+typedef struct {t_sexe sexe; int nourriture; int age;} t_fourmi[];
+typedef enum {unseen=0, seen=1} t_discover;
+typedef enum {vide=0, mur=1, food=2, insecte=3} t_etat;
+typedef struct {t_discover decouvert; t_etat etat; int insecte;} t_lab;
+
+//t_fourmi fourmi;
+
+void decouvrir(int maxx, int maxy, t_lab lab[maxx][maxy]) {
+  int i, j;
+  for(i=0;i<maxx;i++) {
+    for(j=0;j<maxy;j++){
+      if(lab[i][j].etat==insecte) {
+          lab[i][j].decouvert=seen;
+          /* Découverte suivant l'axe des x*/
+          if(i+1<maxx)
+            lab[i+1][j].decouvert=seen;
+          if(i+2<maxx)
+            lab[i+2][j].decouvert=seen;
+          if(i-1>0)
+            lab[i-1][j].decouvert=seen;
+          if(i-2>0)
+            lab[i-2][j].decouvert=seen;
+
+          /*Découverte suivant l'axe des y*/
+          if(j+1<maxy)
+            lab[i][j+1].decouvert=seen;
+          if(j+2<maxy)
+            lab[i][j+2].decouvert=seen;
+          if(i-1>0)
+            lab[i][j-1].decouvert=seen;
+          if(j-2>0)
+            lab[i][j-2].decouvert=seen;
+
+          /* Découverte dans les diagonales */
+          if(i+1<maxx && j+1<maxy)
+            lab[i+1][j+1].decouvert=seen;
+          if(i-1>0 && j-1>0)
+            lab[i-1][j-1].decouvert=seen;
+          if(i+1<maxx && j-1>0)
+            lab[i+1][j-1].decouvert=seen;
+          if(i-1>0 && j+1<maxy)
+            lab[i-0][j+1].decouvert=seen;
+      }
+    }
+  }
+}
