a9278cd8b20cab9d793e41421ab2d221fc7fbf24
keryann <keryannbussereau@gmail.com>
2015-12-09 09:16:11 +0100

    Modifs fonctions pour globalisation

--- fonctions/menu.c
+++ fonctions/menu.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define X 20 //Définit la taille du labyrinthe
+#define Y 20
+
 typedef enum {MALE, FEMELLE} t_sexe;
-typedef struct {t_sexe sexe; int nourriture; int age;} t_fourmi[];
+typedef struct {t_sexe sexe; int nourriture; int age;} t_fourmi;
+t_fourmi fourmi[];
+
 typedef enum {unseen=0, seen=1} t_discover;
 typedef enum {vide=0, mur=1, food=2, insecte=3} t_etat;
 typedef struct {t_discover decouvert; t_etat etat; int insecte;} t_lab;
+t_lab lab[X][Y];
 
-void menu(int maxx, int maxy, t_lab lab[maxx][maxy]) {
+void main(int maxx, int maxy, t_lab lab[maxx][maxy]) {
   int choix;
   // Affichage du menu
   do {
@@ -19,8 +25,8 @@ void menu(int maxx, int maxy, t_lab lab[maxx][maxy]) {
     scanf("%d", &choix);
 
     switch(choix) {
-      case 1 : fonc1() ; break;
-      case 2 : fonc2() ; break;
+      case 1 : regles() ; break;
+      case 2 : lancement() ; break;
       case 3 : break;
 
     }
@@ -29,10 +35,13 @@ void menu(int maxx, int maxy, t_lab lab[maxx][maxy]) {
   printf("Au revoir !\n");
 }
 
-void fonc1() {
-
+void regles() {
+  printf("\n Les règles sont les suivantes :\n");
+  printf("A chaque tour vous pouvez choisir de :\n");
+  printf("- Placer soit 0 ou 1 insecte (géré aléatoirement)\n");
+  printf("- Placer entre 1 et 3 de nourriture (géré aléatoirement)");
 }
 
-void fonc2() {
-
+void lancement() {
+  genelab(lab);
 }
