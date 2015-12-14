411d25116e72fdd94cdf6cd2e910808935cb7574
lboisson <boisson.leo@gmail.com>
2015-12-14 17:24:49 +0100

    blablabla

--- header/main.h
+++ header/main.h
@@ -13,3 +13,5 @@ void deplacement (t_lab lab[X][Y], int pos_x, int pos_y);
 int actionUser(t_lab lab[X][Y], int bouffe, int dureevie, t_fourmi fourmi[], int nb_ins);
 
 int prochain_deplacement (int pos_x, int pos_y, t_lab lab[X][Y], t_fourmi fourmi[], int bouffe, int dureevie, int nb_ins);
+
+void sauver_lab(t_lab lab[X][Y]);
