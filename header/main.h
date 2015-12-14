bbee565a7eeb0589364ee3bf4dd5c29066066cdd
keryann <keryannbussereau@gmail.com>
2015-12-14 14:53:45 +0100

    rangements fichiers

--- header/main.h
+++ header/main.h
@@ -8,8 +8,6 @@ void decouvrir(t_lab lab[X][Y]);
 
 void afficher_lab(t_lab lab[X][Y]);
 
-int verifvictoire(t_lab lab[X][Y]);
-
 void deplacement (t_lab lab[X][Y], int pos_x, int pos_y);
 
 int actionUser(t_lab lab[X][Y], int bouffe, int dureevie, t_fourmi fourmi[], int nb_ins);
--- header/struct_lab.h
+++ header/struct_lab.h
@@ -1,9 +1,6 @@
 #define X 30
 #define Y 60
 
-typedef enum {MALE, FEMELLE} t_sexe;
-typedef struct {t_sexe sexe; int nourriture; int age;} t_fourmi;
-
 typedef enum {unseen=0, seen=1} t_discover;
 typedef enum {vide=' ', mur='#', food='*', insecte='%'} t_etat;
 
