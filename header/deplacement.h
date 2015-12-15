df26cecff02555996332297c9d4f6a19069185eb
keryann <keryannbussereau@gmail.com>
2015-12-15 16:26:20 +0100

    suppression déclarations inutiles

--- header/deplacement.h
+++ header/deplacement.h
@@ -4,10 +4,6 @@ void deplacement (t_lab lab[X][Y], int pos_x, int pos_y, t_fourmi fourmi[]);
 
 int pluscourte_dist(t_lab labyrinthe[X][Y], int xdep, int ydep, int xarr, int yarr);
 
-void gene_ins_deb(int bouffe, int dureevie, t_lab lab[X][Y], t_fourmi fourmi[]);
-
-int gene_ins(int bouffe, int dureevie, t_lab lab[X][Y], int nb_ins, int a, int b, t_fourmi fourmi[]);
-
 int combat(int x, int y, int a, int b, int bouffe, int dureevie, t_lab lab[X][Y], t_fourmi fourmi[]);
 
 void bebe(int bouffe, int dureevie, t_lab lab[X][Y], t_fourmi fourmi[]);
