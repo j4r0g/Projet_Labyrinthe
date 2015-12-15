b3af04233a364130b279108c95efde69ea83ade7
keryann <keryannbussereau@gmail.com>
2015-12-15 15:13:43 +0100

    modif fichiers .h

--- header/deplacement.h
+++ header/deplacement.h
@@ -1,6 +1,6 @@
 #define INFINI 10000
 
-void deplacement (t_lab lab[X][Y], int pos_x, int pos_y);
+void deplacement (t_lab lab[X][Y], int pos_x, int pos_y, t_fourmi fourmi[]);
 
 int pluscourte_dist(t_lab labyrinthe[X][Y], int xdep, int ydep, int xarr, int yarr);
 
@@ -11,3 +11,5 @@ int gene_ins(int bouffe, int dureevie, t_lab lab[X][Y], int nb_ins, int a, int b
 int combat(int x, int y, int a, int b, int bouffe, int dureevie, t_lab lab[X][Y], t_fourmi fourmi[]);
 
 void bebe(int bouffe, int dureevie, t_lab lab[X][Y], t_fourmi fourmi[]);
+
+int modifpos(t_lab lab[X][Y], t_fourmi fourmi[], int x, int y, int a, int b);
